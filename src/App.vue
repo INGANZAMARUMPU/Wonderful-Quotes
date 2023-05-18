17e613dfe3a5fa5631818f3790715521c3ba7cac
INGANZAMARUMPU <jonkur96@gmail.com>
2023-05-18 09:21:31 +0200

    good UX

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
+    <h2>IMYIBUTSA Y'IKIRUNDI</h2>
     <quotes-progress v-bind:quotes="quotes"></quotes-progress>
-    <quotes-field v-on:add_quote="pushQuote"></quotes-field>
     <div class="quotes">
-      <quotesLabel width="25%"
+      <quotesLabel
         v-for="(quote, position) in quotes"
         v-bind:position="position"
-        v-on:click.native="e => remove(e, position)">
+        v-on:close="e => remove(e, position)">
         {{ quote }}
       </quotesLabel>
     </div>
     <div class="info">
-      <span>ehe nkwereke: ufyonze ku mwibutsa uba uwuhanaguye!</span>
+      <div>Menya Neza:
+        <ul>
+          <li>
+            Ntushobora gushiramwo umwibutsa ugaragara!
+          </li>
+          <li>
+            Ufyonze ku kamenyetso &times uba uhanaguye umwibutsa!
+          </li>
+          <li>
+            Usigaje gushiramwo imyibutsa {{ 10 - this.quotes.length }}
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -39,9 +51,9 @@ export default {
       this.quotes.push(quote);
     },
     remove(event, position){
-      event.target.classList.add("hidden")
+      event.target.closest('h3').classList.add("hidden")
       setTimeout(() => {
-        event.target.classList.remove("hidden")
+        event.target.closest('h3').classList.remove("hidden")
         this.quotes.splice(position, 1)
       }, 1000)
     }
@@ -54,20 +66,28 @@ export default {
   max-width: 1000px;
   margin: auto;
 }
+h2{
+  text-align: center;
+}
 .info{
   background-color: #ddf0ff;
-  text-align: center;
+  color: #5590aa;
   padding-top: 1em;
   padding-bottom: 1em;
   border: 1px solid #acd;
   border-radius: 3px;
+  padding: 10px 40px 0 40px;
 }
 *{
   font-family: sans-serif;
   box-sizing: border-box;  
 }
 .quotes{
+  width: 100%;
   margin-bottom: 10px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
 }
 .hidden{
   opacity: 0;
--- src/Quotes/QuotesLabel.vue
+++ src/Quotes/QuotesLabel.vue
@@ -1,35 +1,52 @@
 <template>
-  <div id="main" v-bind:style="{width:width}">
-  	<h3>
-  		<slot>no text</slot>
-  	</h3>
-  </div>
-</template>
-<script>
-export default {
-	props :{
-		position:{
-			type : Number,
-			default : -1
-		},
-		width : String
-	}
-};
-</script>
-<style scoped>
-#main{
-	display: inline-block;
-}
-h3{
-	color: gray;
-	border: 2px solid lightgray;
-	border-radius: 5px; 
-	font-style: italic;
-	padding: 1em;
-	margin: 5px;
-	word-break: break-all;
-}
-h3:hover{
-	background-color: #fee;
-}
-</style>
+	<div v-bind:style="{width:width}">
+		<h3>
+			<slot>no text</slot>
+		  <div class="close" @click="e => $emit('close', e)">&times</div>
+		</h3>
+	</div>
+  </template>
+  <script>
+  export default {
+	  props :{
+		  position:{
+			  type : Number,
+			  default : -1
+		  },
+		  width : String
+	  }
+  };
+  </script>
+  <style scoped>
+  h3{
+	  background-color: #dfd;
+	  color: #7a7;
+	  border: 2px solid #7a7;
+	  border-radius: 5px; 
+	  font-style: italic;
+	  padding: 1em;
+	  word-break: break-all;
+	  box-shadow: 0 0 2px#7a7;
+	  display: flex;
+	  justify-content: space-between;
+	  margin: 0;
+  }
+  h3:hover{
+	  background-color: #fee;
+	  border: 2px solid #a77;
+	  color: #a77;
+	  box-shadow: 0 0 5px#a77;
+  }
+  .close{
+	  display: none;
+  }
+  h3:hover .close {
+	  display: block;
+  } 
+  .close:hover{
+	  cursor: default;
+	  color: red;
+	  text-shadow: 0 0 10px red;
+  }
+  </style>
+  
